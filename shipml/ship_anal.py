9f6c7a70a24400203f32a7c9fa7fd0588c0c5985
Evann9 <bluefrogg9@gmail.com>
2026-04-21 22:37:43 +0900

    수정

--- shipml/ship_anal.py
+++ shipml/ship_anal.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
